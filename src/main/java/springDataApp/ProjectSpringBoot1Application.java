1657fc17651c0cb2dc980a0243d54e65ff1d9da0
zineb <zineby100@gmail.com>
2024-04-26 20:00:55 +0200

    first

---
+++ src/main/java/springDataApp/ProjectSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package springDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBoot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/springDataApp/ProjectSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
