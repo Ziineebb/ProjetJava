7b315430d02f92e9272f29841e8bfd915467e4b9
ayabelaoui <ayabelaoui1@gmail.com>
2024-04-27 23:21:05 +0200

    update classe :Election,Vote,Resultat

--- src/main/java/springDataApp/dao/Election.java
+++ src/main/java/springDataApp/dao/Election.java
@@ -1,7 +1,26 @@
 package springDataApp.dao;
 
-public class Election {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Election {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String titre;
+	private String type;
+	private String dateDebut;
+	private String dateFin;
+	private String status;
 	
 	
 	
--- src/main/java/springDataApp/dao/Resultat.java
+++ src/main/java/springDataApp/dao/Resultat.java
@@ -1,7 +1,24 @@
 package springDataApp.dao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 
 public class Resultat {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private Election e;
+	private Candidat c;
+	private int nbreVote;
 
 }
